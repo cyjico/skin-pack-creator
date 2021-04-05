b3d8af2b9dd076d6e01ac760074ffffef96c1e9a
marahuyo <going2email2@gmail.com>
2021-04-06 00:52:43 +0800

    Add ability to close modal; Fix appending bug

--- js/alert-modal.js
+++ js/alert-modal.js
@@ -1,20 +1,29 @@
+function alertModal(message = "") {
+  const modal = document.createElement("div");
+  modal.classList.add("modal");
 
-function alertModal(message='') {
-  const modal = document.createElement('div');
-  modal.classList.add('modal');
+  const modalContent = document.createElement("div");
+  modalContent.classList.add("modal-content");
+  modal.appendChild(modalContent);
 
-  const modalContent = document.createElement('div');
-  modalContent.classList.add('modal-content');
-  modal.appendChild(modal);
-
-  const header = document.createElement('h3');
+  const header = document.createElement("h3");
   header.innerText = message;
   modalContent.appendChild(header);
 
-  const button = document.createElement('button');
-  button.classList.add('modal-close');
+  const button = document.createElement("button");
+  button.innerText = "X";
+  button.classList.add("modal-close");
   header.appendChild(button);
-  
+
+  button.addEventListener("click", () => {
+    modal.remove();
+  });
+  modal.addEventListener("click", (ev) => {
+    if (ev.target === modal) {
+      modal.remove();
+    }
+  });
+
   document.body.appendChild(modal);
 }
 
